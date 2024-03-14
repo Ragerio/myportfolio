6203cd9cb8674b05a80b99caf078c43d5830585f
Matt <rhoen530@gmail.com>
2024-03-14 12:49:42 -0700

    created see more functionality

--- js/site-common.js
+++ js/site-common.js
@@ -1,23 +1,6 @@
-$( document ).ready(function() {
-    
-   function animatethis(targetElement, speed) {
-        $(targetElement).animate({ marginLeft: "-=10px"},
-        {
-            duration: speed,
-            complete: function ()
-            {
-                targetElement.animate({ marginLeft: "+=10px" },
-                {
-                    duration: speed,
-                    complete: function ()
-                    {
-                        animatethis(targetElement, speed);
-                    }
-                });
-            }
-        });
-    };
-
-//    animatethis($('#knight'), 1000);
-
+$(document).ready(function() {
+    $('#see-more').on('click', function() {
+        $('.card-container-ex').slideDown('slow');
+        $(this).hide();
+    });
 });
